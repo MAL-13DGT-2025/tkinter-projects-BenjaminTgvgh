572badf31f07ba8ae4ec667de76c9ad0e633d483
BenjaminTgvgh <Benjamin.tuangvungh@marcellin.school.nz>
2025-03-06 12:30:18 +1300

    Calculator program

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+import tkinter as tk  #This is needed 
+from tkinter import ttk
+
+root = tk.Tk() #I need this
+
+calc = ttk.Entry(root)
+calc.grid(row = 0, column = 0, columnspan = 4)
+
+b_7 = ttk.Button(root, text )
+
+root.mainloop() #Must have 
